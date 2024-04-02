9573dc1c4b168fe5ac88669b274536bec73edd53
kræn byskov <kraenbyskov@gmail.com>
2024-04-02 15:27:57 +0200

    added react native paper

--- App.tsx
+++ App.tsx
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { PaperProvider, MD3LightTheme as DefaultTheme, Button } from 'react-native-paper';
+import { name } from './app.json';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
 
 export default function App() {
   return (
+      <PaperProvider theme={theme}>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
       <StatusBar style="auto" />
     </View>
+      </PaperProvider>
   );
 }
+
+
+AppRegistry.registerComponent(name, () => App);
 
 const styles = StyleSheet.create({
   container: {
